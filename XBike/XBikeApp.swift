ce097313d383c7f917fb80f5bad113eeca29ef98
Alex Hernandez <ivan.hdez1984@gmail.com>
2025-03-28 17:45:01 -0700

    Initial Commit

---
+++ XBike/XBikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XBikeApp.swift
+//  XBike
+//
+//  Created by Ivan Alejandro Hernandez Sanchez on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct XBikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
